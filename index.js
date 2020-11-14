dea8325650f458f8ad1559756477557d1533fafa
yfujii01 <yfujii01@gmail.com>
2020-11-14 14:39:49 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-const ImgParam = class {
-  constructor(ctx, x, y, width, height, radius, image) {
-    this.ctx = ctx;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.radius = radius;
-    this.image = image;
-  }
+const P = {
+  x: 10,
+  y: 10,
+  w: 406,
+  h: 879,
+  r: 60,
 };
 
 const Point = class {
@@ -17,10 +13,47 @@ const Point = class {
   }
 };
 
+function makeImage() {
+  const board = document.querySelector("#board");
+  const ctx = board.getContext("2d");
+  drawFrame(ctx);
+
+  // 画像読み込み
+  loadImageAndDraw(ctx);
+}
+
+function loadImageAndDraw(ctx) {
+  const image = new Image();
+  const oFReader = new FileReader();
+  const fileDom = document.getElementById("uploadImage");
+  if (fileDom.files.length > 0) {
+    oFReader.readAsDataURL(fileDom.files[0]);
+
+    oFReader.onload = function (oFREvent) {
+      image.onload = function () {
+        drawScreen(ctx, image);
+      };
+      image.src = oFREvent.target.result;
+    };
+  } else {
+    drawScreen(ctx, image);
+  }
+}
+
+function drawScreen(ctx, image) {
+  ctx.drawImage(image, P.x, P.y, P.w, P.h);
+
+  drawNotch(ctx);
+  drawSpeaker(ctx);
+  drawCamera(ctx);
+}
+
+window.onload = () => {
+  makeImage();
+};
+
 function fileChanged(input) {
-  console.log(input);
   if (input.files.length > 0) {
-    console.log(input.files[0]);
     makeImage();
   }
 }
@@ -35,7 +68,6 @@ function drawBox(ctx, x, y, w, h, r) {
   const p7 = new Point(x, y + h - r);
   const p8 = new Point(x, y + r);
 
-  // 枠
   ctx.beginPath();
   ctx.moveTo(p1.x, p1.y); // 左上(1)
   ctx.lineTo(p2.x, p2.y); // 左上→右上(1->2)
@@ -47,114 +79,62 @@ function drawBox(ctx, x, y, w, h, r) {
   ctx.lineTo(p8.x, p8.y); // 左下→左上(7->8)
   ctx.quadraticCurveTo(p8.x, p1.y, p1.x, p1.y); // 左上カーブ(8->1)
   ctx.closePath();
-
   ctx.stroke();
 }
 
-function drawCircle(ctx, x, y, w, h, r) {
+function drawCircle(ctx, x, y, h) {
   ctx.beginPath();
-
-  ctx.arc(x, y, h, 0, Math.PI * 2, true); // 外の円
-
+  ctx.arc(x, y, h, 0, Math.PI * 2, true);
   ctx.closePath();
   ctx.stroke();
 }
 
-function drawFrame(prm) {
-  prm.ctx.strokeStyle = "rgb(0, 0, 0)";
-  prm.ctx.lineWidth = 20;
+function drawFrame(ctx) {
+  ctx.strokeStyle = "rgb(0, 0, 0)";
+  ctx.lineWidth = 20;
 
-  const x = prm.x;
-  const y = prm.y;
-  const w = prm.width;
-  const h = prm.height;
-  const r = prm.radius;
-  drawBox(prm.ctx, x, y, w, h, r);
-  prm.ctx.clip();
+  const x = P.x;
+  const y = P.y;
+  const w = P.w;
+  const h = P.h;
+  const r = P.r;
+  drawBox(ctx, x, y, w, h, r);
+  ctx.clip();
 }
 
-function drawSpeaker(prm) {
-  prm.ctx.strokeStyle = "rgb(50, 50, 50)";
-  prm.ctx.lineWidth = 5;
+function drawSpeaker(ctx) {
+  ctx.strokeStyle = "rgb(50, 50, 50)";
+  ctx.lineWidth = 5;
 
   const width_ratio = 2.3;
-  const x = prm.x + prm.width / width_ratio;
-  const y = prm.y + prm.height / 60;
-  const w = prm.width - (prm.width / width_ratio) * 2;
-  const h = prm.height / 180;
-  const r = prm.radius / 12;
-  drawBox(prm.ctx, x, y, w, h, r);
+  const x = P.x + P.w / width_ratio;
+  const y = P.y + P.h / 60;
+  const w = P.w - (P.w / width_ratio) * 2;
+  const h = P.h / 180;
+  const r = P.r / 12;
+  drawBox(ctx, x, y, w, h, r);
 }
 
-function drawCamera(prm) {
-  prm.ctx.strokeStyle = "rgb(0, 40, 40)";
-  prm.ctx.lineWidth = 5;
+function drawCamera(ctx) {
+  ctx.strokeStyle = "rgb(0, 40, 40)";
+  ctx.lineWidth = 5;
 
   const width_ratio = 1.65;
-  const x = prm.x + prm.width / width_ratio;
-  const y = prm.y + prm.height / 52;
-  const w = prm.width - (prm.width / width_ratio) * 2;
-  const h = prm.height / 500;
-  const r = 5;
-  drawCircle(prm.ctx, x, y, w, h, r);
+  const x = P.x + P.w / width_ratio;
+  const y = P.y + P.h / 52;
+  const h = P.h / 500;
+  drawCircle(ctx, x, y, h);
 }
 
-function drawNotch(prm) {
-  prm.ctx.strokeStyle = "rgb(0, 0, 0)";
-  prm.ctx.lineWidth = 40;
+function drawNotch(ctx) {
+  ctx.strokeStyle = "rgb(0, 0, 0)";
+  ctx.lineWidth = 40;
 
   const width_ratio = 3.5;
-  const x = prm.x + prm.width / width_ratio;
-  const y = prm.y;
-  const w = prm.width - (prm.width / width_ratio) * 2;
-  const h = prm.height / 60;
-  const r = prm.radius / 60;
-  drawBox(prm.ctx, x, y, w, h, r);
+  const x = P.x + P.w / width_ratio;
+  const y = P.y;
+  const w = P.w - (P.w / width_ratio) * 2;
+  const h = P.h / 60;
+  const r = P.r / 60;
+  drawBox(ctx, x, y, w, h, r);
 }
-
-function makeImage() {
-  const board = document.querySelector("#board");
-  const ctx = board.getContext("2d");
-
-  const x = 10;
-  const y = 10;
-  const width = 406;
-  const height = 879;
-  const radius = 60;
-
-  const imgParam = new ImgParam(ctx, x, y, width, height, radius, new Image());
-
-  drawFrame(imgParam);
-
-  // 画像読み込み
-  loadImageAndDraw(imgParam);
-}
-
-function loadImageAndDraw(prm) {
-  const oFReader = new FileReader();
-  const fileDom = document.getElementById("uploadImage");
-  if (fileDom.files.length > 0) {
-    oFReader.readAsDataURL(fileDom.files[0]);
-
-    oFReader.onload = function (oFREvent) {
-      prm.image.onload = function () {
-        drawScreen(prm);
-      };
-      prm.image.src = oFREvent.target.result;
-    };
-  } else {
-    drawScreen(prm);
-  }
-}
-
-function drawScreen(prm) {
-  prm.ctx.drawImage(prm.image, prm.x, prm.y, prm.width, prm.height);
-
-  drawNotch(prm);
-  drawSpeaker(prm);
-  drawCamera(prm);
-}
-
-window.onload = () => {
-  makeImage();
-};
